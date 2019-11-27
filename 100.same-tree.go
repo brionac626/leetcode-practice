182e2b751e7e6cde6dae76ab9dc46f3aa39764c2
AutumnAoi <autumnaoi@starlordtech.com>
2019-11-27 10:11:22 +0800

    init problem

---
+++ 100.same-tree.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=100 lang=golang
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    
+}
+// @lc code=end
+
---
+++ 171.excel-sheet-column-number.go
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=171 lang=golang
+ *
+ * [171] Excel Sheet Column Number
+ */
+
+// @lc code=start
+func titleToNumber(s string) int {
+    
+}
+// @lc code=end
+
